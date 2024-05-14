f8a7869df4a31975904db7fdb71002be86bfd3fb
gao.zhiyuan <gao.zhiyuan@outlook.com>
2024-05-14 23:43:28 +0800

    update

--- art-school-booking-system/src/components/CourseRegistration.vue
+++ art-school-booking-system/src/components/CourseRegistration.vue
@@ -1,50 +1,85 @@
 <template>
-  <el-container class="course-registration" direction="vertical">
-    <div class="course-selection">
-      <h2 class="course-title">已经预约的课程</h2>
-      <el-select v-model="selectedCourse" @change="updateTotalPrice" placeholder="请选择" class="course-menu">
-        <el-option
-          v-for="course in courses"
-          :key="course.id"
-          :label="course.courseTitle + ' - ' + formatCurrency(course.price)"
-          :value="course.id">
-        </el-option>
-      </el-select>
-    </div>
+    <!-- 使用el-row和el-col创建水平布局 -->
+    <el-row :gutter="20">
+      <!-- 左侧部分显示课程卡片与分页 -->
+      <el-col :span="15">
+        <el-main>
+          <h2 class="course-title">已经预约的课程</h2>
+            <CourseCard  v-for="course in pagedResults"
+            :key="course.id"
+            :searchResults="[course]">
+            </CourseCard>
+    
+            <transition name="fade">
+              <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage"
+                :page-size="pageSize"
+                layout="total, prev, pager, next, jumper"
+                :total="searchResults.length">
+              </el-pagination>
+            </transition>
+        </el-main>
+      </el-col>
 
-    <h2>价格总计: {{ formatCurrency(totalPrice) }}</h2>
-
-    <el-form class="registration-form" >
-      <form @submit.prevent="handleSubmit()">
-        <h2>填写个人信息</h2>
-        <el-input v-model="registrationInfo.name" placeholder="姓名" required class="form-input"></el-input>
-        <el-input v-model="registrationInfo.email" type="email" placeholder="邮箱" required class="form-input"></el-input>
-        <el-input v-model="registrationInfo.notes" type="textarea" placeholder="备注" class="form-input"></el-input>
-        <el-button type="primary" native-type="submit">提交</el-button>
-      </form>
-    </el-form>
-
-    <el-dialog v-model="showPaymentScreen" class="payment-dialog">
-      <h2 class="payment-title">支付详情</h2>
-      <p class="payment-detail">课程: {{ courseTitle }}</p> 
-      <p class="payment-detail">购买人姓名: {{ registrationInfo.name }}</p> 
-      <p class="payment-detail">邮箱: {{ registrationInfo.email }}</p> 
-      <!-- 在这里集成实际的支付系统组件 -->
-      <p class="payment-instruction">请继续以完成支付...</p>
-      <el-button class="payment-button" type="primary" @click="processPayment(true,courseTitle)">支付成功</el-button>
-      <el-button class="payment-button" type="danger" @click="processPayment(false,courseTitle)">支付失败</el-button>
-      <template v-slot:footer>
-        <el-button class="close-button" @click="showPaymentScreen = false">关闭</el-button>
-      </template>
-    </el-dialog>
-  </el-container>
+      <el-divider direction="vertical"></el-divider>
+     
+      <!-- 右侧部分显示已经预约的课程、个人信息表单和支付信息 -->
+      <el-col :span="7">
+        <el-main>
+          <div class="course-selection">
+            <h2 class="course-title">已经预约的课程</h2>
+            <el-select v-model="selectedCourse" @change="updateTotalPrice" placeholder="请选择" class="course-menu">
+              <el-option
+                v-for="course in courses"
+                :key="course.id"
+                :label="course.courseTitle + ' - ' + formatCurrency(course.price)"
+                :value="course.id">
+              </el-option>
+            </el-select>
+          </div>
+    
+          <h2>价格总计: {{ formatCurrency(totalPrice) }}</h2>
+    
+          <el-form class="registration-form" >
+            <form @submit.prevent="handleSubmit()">
+              <h2>填写个人信息</h2>
+              <el-input v-model="registrationInfo.name" placeholder="姓名" required class="form-input"></el-input>
+              <el-input v-model="registrationInfo.email" type="email" placeholder="邮箱" required class="form-input"></el-input>
+              <el-input v-model="registrationInfo.notes" type="textarea" placeholder="备注" class="form-input"></el-input>
+              <el-button type="primary" native-type="submit">提交</el-button>
+            </form>
+          </el-form>
+    
+          <el-dialog v-model="showPaymentScreen" class="payment-dialog">
+            <h2 class="payment-title">支付详情</h2>
+            <p class="payment-detail">课程: {{ courseTitle }}</p> 
+            <p class="payment-detail">购买人姓名: {{ registrationInfo.name }}</p> 
+            <p class="payment-detail">邮箱: {{ registrationInfo.email }}</p>  
+            <!-- 在这里集成实际的支付系统组件-->
+            <p class="payment-instruction">请继续以完成支付...</p>
+            <el-button class="payment-button" type="primary" @click="processPayment(true,courseTitle)">支付成功</el-button>
+            <el-button class="payment-button" type="danger" @click="processPayment(false,courseTitle)">支付失败</el-button>
+            <template v-slot:footer>
+              <el-button class="close-button" @click="showPaymentScreen = false">关闭</el-button>
+            </template>
+          </el-dialog>
+        </el-main>
+      </el-col>
+    </el-row>
 </template>
 
 <script>
 import axios from 'axios';
+import CourseCard from './CourseCard.vue';
+
 
   export default {
     name: 'CourseRegistration',
+    components: {
+     CourseCard,
+    },
     data() {
       return {
         courses: [],
@@ -58,6 +93,11 @@ import axios from 'axios';
         },
         showPaymentScreen: false,
         username:null,
+        searchResults: [],
+        pagedResults: [],
+        pageSize: 12,
+        currentPage: 1,
+
         
       };
     },
@@ -67,6 +107,21 @@ import axios from 'axios';
       this.fetchCourses();
     },
     methods: {
+      handlePagination() {
+        const start = (this.currentPage - 1) * this.pageSize;
+        const end = start + this.pageSize;
+        this.pagedResults = this.searchResults.slice(start, end);
+      },
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.handlePagination();
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.handlePagination();
+      },
+
+
       fetchCourses() {
         const users = this.username;
         axios.get(`http://localhost:3000/api/courseregistration`, {
@@ -76,12 +131,15 @@ import axios from 'axios';
         })
         .then(response => {
           this.courses = response.data;
+          this.searchResults = response.data
+          this.handlePagination();
         })
         .catch(error => {
           console.error(error);
           // 添加错误处理
         })
       },
+      
       updateTotalPrice() {
         if (this.selectedCourse) {
           const course = this.courses.find(c => c.id === this.selectedCourse);
@@ -153,25 +211,9 @@ import axios from 'axios';
   </script>
   
 <style scoped>
-  .course-selection {
-    display: flex;
-    align-items: center;
+  .el-divider--vertical {
+    height: auto; /* 使分割线垂直延伸 */
   }
-
-  .course-title {
-    margin-right: 10px;
-    white-space: nowrap;
-  }
-
-  .course-menu {
-    flex-grow: 1;
-  }
-
-  .el-container {
-  width: 25%;  /* 设置宽度为50% */
-  margin: 0 auto;  /* 居中对齐 */
-  }
-
   .form-input {
     width: 100%;  /* 你可以将 100% 替换为具体的像素值或其他单位 */
     font-size: 16px;  /* 调整字体大小 */
@@ -180,7 +222,7 @@ import axios from 'axios';
   .text-area {
     height: 200px;  /* 针对文本区域调整高度 */
   }
-
+/* 
   .payment-dialog {
     background: #f5f5f5;
     border-radius: 10px;
@@ -205,5 +247,5 @@ import axios from 'axios';
   .close-button {
     background-color: #e6a23c;
     color: white;
-  }
+  } */
 </style>
